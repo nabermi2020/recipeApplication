3456fc1fa8e6000c67709fcf5de7591f39a1cb72
M.Naberezhnyi <naber201725@gmail.com>
2020-02-14 16:13:36 +0200

    Basic actions for the recipes store were added

--- src/app/recipes/store/recipe.actions.ts
+++ src/app/recipes/store/recipe.actions.ts
@@ -1,3 +1,38 @@
 import { Action } from '@ngrx/store';
+import { Recipe } from '../recipe.model';
 
-export type RecipeActions = '';
+// export const ADD_INGREDIENTS = '[RECIPES] ADD_INGREDIENTS';
+export const ADD_RECIPE = '[RECIPES] ADD_INGREDIENTS';
+export const SET_RECIPES = '[RECIPES] SET_RECIPES'; 
+export const DELETE_RECIPE = '[RECIPES] DELETE_RECIPES';
+export const UPDATE_RECIPE = '[RECIPES] UPDATE_RECIPE';
+
+export class SetRecipes implements Action {
+    readonly type = SET_RECIPES;
+    constructor(public payload: Array<Recipe>) {}
+}
+
+export class AddRecipe implements Action {
+    readonly type = ADD_RECIPE;
+    constructor(public payload: Recipe) {}
+}
+
+export class UpdateRecipe implements Action {
+    readonly type = UPDATE_RECIPE;
+    constructor(public payload: {index: number, recipe: Recipe}) {}
+}
+
+export class DeleteRecipe implements Action {
+    readonly type = DELETE_RECIPE;
+    constructor(public payload: number) {}
+}
+
+// export class AddIngredients implements Action {
+//     readonly type = DELETE_RECIPE;
+//     constructor(public payload: number) {}
+// }
+
+export type RecipeActions = SetRecipes | 
+                            AddRecipe |
+                            UpdateRecipe |
+                            DeleteRecipe;
